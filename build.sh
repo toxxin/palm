f46b0ba173dc4ca61f36cb56f9f8b93d9bf829e3
Anton Glukhov <toxxin@mail.ru>
2015-06-03 23:45:48 +0300

    Rename and update build script

--- build.sh
+++ build.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
 LOADADDR=0x80200000
-SOURCE_PATH=./kernel_palm/
+KERNEL_SOURCE_PATH=./kernel_palm/
+BOOTLOADER_SOURCE_PATH=./uboot_palm/
 TFTP_PATH=/tftpboot/
 export PATH=$PATH:/home/anton/Source/nmv/uboot_palm/tools/
 export ARCH=arm
 export CROSS_COMPILE=/home/anton/Distrib/arm-2013.05/bin/arm-none-linux-gnueabi-
 
-cd ${SOURCE_PATH}
+cd ${BOOTLOADER_SOURCE_PATH}
+make -j8
+cd ..
+
+cd ${KERNEL_SOURCE_PATH}
 make -j8 && \
 cat ./arch/arm/boot/zImage ./arch/arm/boot/dts/nmv.dtb > zImage-with-dtb && \
 mkimage -A arm -O linux -T kernel -C none -a ${LOADADDR} -e ${LOADADDR} -d zImage-with-dtb ${TFTP_PATH}uImage
